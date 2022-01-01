7aa636250bf1d01fc9144cce0816d9233fca823b
Derek Yu <derek-nis@hotmail.com>
2022-01-01 13:27:38 -0500

    Add in_memory_database_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,11 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'pry'
+require 'relation_to_json'
+
+require_relative 'helpers/in_memory_database_helper'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest

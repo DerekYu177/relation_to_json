0ba7fdb8670ca128edcae079c3d624c9d13acd55
Derek Yu <derek-nis@hotmail.com>
2022-01-02 11:53:35 -0500

    Add specs for multiple belongs_to of the same class but with different association names

--- spec/relation_to_json/base_model_with_belongs_to_spec.rb
+++ spec/relation_to_json/base_model_with_belongs_to_spec.rb
@@ -8,6 +8,14 @@ class FakeOverlord < ActiveRecord::Base
   has_one :fake_employee
 end
 
+class FakeAccount < ActiveRecord::Base
+end
+
+class FakeMessage < ActiveRecord::Base
+  belongs_to :sender, class_name: 'FakeAccount'
+  belongs_to :receiver, class_name: 'FakeAccount'
+end
+
 describe(RelationToJSON::Base) do
   context 'with belongs to relation' do
     create_new_database_with do
@@ -46,6 +54,57 @@ describe(RelationToJSON::Base) do
             "fake_overlord" => {
               "id" => n+1,
               "name" => "Company#{n}"
+            }
+          }
+        end
+
+        expect(subject).to(eq(expected))
+      end
+    end
+  end
+
+  context 'with multiple belongs_to of the same class' do
+    create_new_database_with do
+      create_table :fake_accounts do |t|
+        t.string :first_name
+      end
+
+      create_table :fake_messages do |t|
+        t.integer :sender_id
+        t.integer :receiver_id
+      end
+    end
+
+    describe(FakeMessage, type: :model) do
+      before do
+        5.times do |n|
+          FakeMessage.find_or_create_by(id: n+1) do |message|
+            sender = FakeAccount.find_or_create_by(first_name: "FirstName::Sender::#{n}")
+            receiver = FakeAccount.find_or_create_by(first_name: "FirstName::Receiver::#{n}")
+            message.update!(sender: sender, receiver: receiver)
+          end
+        end
+      end
+
+      let(:relation) { FakeMessage.all }
+
+      subject { RelationToJSON::Base.new(relation, schema).as_json }
+
+      let(:schema) do
+        [ sender: [ :first_name ], receiver: [ :first_name ] ]
+      end
+
+      it 'grabs all of the specified attributes' do
+        expected = 5.times.map do |n|
+          {
+            "id" => n+1,
+            "sender" => {
+              "first_name" => "FirstName::Sender::#{n}",
+              "id" => (2*n)+1,
+            },
+            "receiver" => {
+              "first_name" => "FirstName::Receiver::#{n}",
+              "id" => (2*n)+2,
             }
           }
         end

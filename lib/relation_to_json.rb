8e3d8a64653d2e3308489c49098b07d2d2084ead
Derek Yu <derek-nis@hotmail.com>
2022-01-15 19:43:32 -0500

    Require version

--- lib/relation_to_json.rb
+++ lib/relation_to_json.rb
@@ -2,6 +2,7 @@ require_relative "relation_to_json/base"
 require_relative "relation_to_json/base_reflection"
 require_relative "relation_to_json/belongs_to_reflection"
 require_relative "relation_to_json/has_one_reflection"
+require_relative "relation_to_json/version"
 
 class InvalidSchemaError < StandardError
   def initialize(invalid_attributes, klass)
